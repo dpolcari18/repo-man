dd08590fdf36a2593f2c6174c0911a4bdaeb56b5
David Polcari <dpolcari18@gmail.com>
2021-05-04 11:41:37 -0500

    client side routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import './App.css';
 
+// react router
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
 // react-bootstrap
 import Container from 'react-bootstrap/Container'
 
 function App() {
   return (
-    <Container>
-      <div>TESTING</div>
-    </Container>
+    <Router>
+      <Container>
+        <Route exact patch='/' render={() => <Login />} />
+        <div>TESTING</div>
+      </Container>
+    </Router>
   );
 }
 
