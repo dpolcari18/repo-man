6d7326417bbafc24a501c12ce910141ff6857b36
David Polcari <dpolcari18@gmail.com>
2021-05-04 14:12:10 -0500

    added env filed

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
+## Installation
+
+* Fork and Clone Repo
+* 
+
+
+
+
+
+
+
+
+
+///////////////////////////////////////////////////////////
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,31 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 // react bootstrap
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+
+// GitHub GraphQL endpoint
+const API_ENDPOINT = 'https://api.github.com/graphql'
 
 const Login = () => {
-    return (
-        <Form>
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label>Email address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
-                <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-                </Form.Text>
-            </Form.Group>
 
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label>Password</Form.Label>
-                <Form.Control type="password" placeholder="Password" />
-            </Form.Group>
-            <Form.Group controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-                Submit
-            </Button>
-        </Form>
+    // local state for form
+    const [username, setUsername] = useState('')
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log('Login')
+    }
+
+    return (
+        <Container fluid='xl'>
+            <Form onSubmit={(e) => handleSubmit(e)}>
+                <Form.Group controlId="formBasicEmail">
+                    <Form.Label>GitHub Username</Form.Label>
+                    <Form.Control type="text" placeholder="Enter username" value={username} onChange={(e) => setUsername(e.target.value)}/>
+                </Form.Group>
+
+                {/* <Form.Group controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" />
+                </Form.Group> */}
+                <Button variant="primary" type="submit">
+                    Login
+                </Button>
+            </Form>
+        </Container>
     )
 }
 
