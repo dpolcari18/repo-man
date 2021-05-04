02938e469eb6b2ae7aa7f3f74ba78ff6daaaaff0
David Polcari <dpolcari18@gmail.com>
2021-05-04 12:04:41 -0500

    added login form

--- src/App.js
+++ src/App.js
@@ -3,9 +3,6 @@ import './App.css';
 // react router
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-// react-bootstrap
-import Container from 'react-bootstrap/Container'
-
 // components
 import Login from './components/Login'
 import NotFound from './components/NotFound'
