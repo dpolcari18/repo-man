3c5124f412bb8965e669fe545c090fa987de4284
David Polcari <dpolcari18@gmail.com>
2021-05-13 10:58:00 -0500

    delete repo working

--- src/components/DeleteButton.js
+++ src/components/DeleteButton.js
@@ -7,12 +7,30 @@ import { useSelector } from 'react-redux'
 // react bootstrap
 import Button from 'react-bootstrap/Button'
 
+// credentials
+const TOKEN = process.env.REACT_APP_OAUTH_TOKEN
+
 const DeleteButton = () => {
 
     const reposToDelete = useSelector(state => state.repos.checkedRepos)
 
-    const deleteRepos = async () => {
-        reposToDelete.forEach(repo => console.log(repo))
+    const deleteRepos = () => {
+        
+        const delMethod = async (repo) => {
+            const delObj = {
+                headers: {
+                    "Accept": 'application/vnd.github.v3+json',
+                    "Authorization": `token ${TOKEN}`
+                },
+                method: "DELETE"
+            }
+            const deleteRepo = await fetch(`https://api.github.com/repos/dpolcari18/${repo}`, delObj)
+    
+        }
+        
+        reposToDelete.forEach(repo => {
+            delMethod(repo)
+        })
     }
 
     return (
